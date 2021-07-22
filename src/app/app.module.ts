74c54bc3c4d121f6d3055946300a2b54af1f78ad
Francesco <francescosanti123@gmail.com>
2021-07-22 16:41:52 +0200

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,16 @@ import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { TestComponent } from './test/test.component';
-import { Test2Component } from './test2/test2.component';
-import { NavComponent } from './nav/nav.component';
-import { AboutMeComponent } from './about-me/about-me.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    TestComponent,
-    Test2Component,
-    NavComponent,
-    AboutMeComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'test', component: TestComponent },
-      { path: 'test2', component: Test2Component },
-      { path: 'aboutMe', component: AboutMeComponent}
+      { path: '', component: HomeComponent }
 
     ])
   ],
